e4bcbb4c48af568dccfa6bcd6d90a4fbd0b0ee6c
ZestyyPNW <nos@cfx-server>
2025-12-06 21:20:13 +0000

    feat: leo-vehicles

---
+++ resources/[game-assets]/[leo-vehicles]/LASDVehicles/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'data/**/*.meta',
+}
+
+data_file 'handling_file' 'data/**/handling.meta'
+data_file 'vehicle_metadata_file' 'data/**/vehicles.meta'
+data_file 'carcols_file' 'data/**/carcols.meta'
+data_file 'vehicle_variation_file' 'data/**/carvariations.meta'
+data_file 'vehicle_layouts_file' 'data/**/vehiclelayouts.meta'
+data_file 'WEAPONINFO_FILE' 'data/**/weapon_annm.meta'
+
+client_script {
+	'data/client.lua'
+}
