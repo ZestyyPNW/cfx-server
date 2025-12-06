0a17832c739869251a3ae5fa0d9fe3a30b9ad8fd
ZestyyPNW <nos@cfx-server>
2025-12-06 21:21:49 +0000

    feat: facialhair

---
+++ resources/[game-assets]/[clothing]/facialhair/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'YourName'
+description 'MP Male Beard Overlays'
+version '1.0.0'
+
+files {
+    'stream/*.ytd'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/*.ytd'
